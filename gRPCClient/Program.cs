d2ffc5e46b5c1e341c6b0980405b9dc69f70240d
Ali <a.bolhasani481@gmail.com>
2022-11-27 08:32:37 +0330

    Initial commit

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Core;
+using gRPCClient;
+
+Console.WriteLine("Hello, World!");
+var channel = new Channel("localhost:5001", ChannelCredentials.Insecure);
+var client = new Time.TimeClient(channel);
+var response = client.whatTimeItIs(new
+    EmptyTime()
+    {
+    }
+);
+
+Console.WriteLine(response.ToString());
---
+++ gRPCSample/Services/TimeService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace gRPCSample.Services;
+
+public class TimeService : Time.TimeBase
+{
+    public override Task<getTime> whatTimeItIs(EmptyTime request, ServerCallContext context)
+    {
+        return Task.FromResult(new getTime
+            ()
+            {
+                Day = DateTime.Now.Day.ToString(),
+                Year = DateTime.Now.Year.ToString()
+            });
+    }
+}
